d01e6e76569d4d7f87573ccb948972627f54ace2
Telegram Bot Builders <buzzbuilder0@gmail.com>
2024-07-24 01:50:07 -0400

    Created webhook endpoint

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
+@app.route('/webhook')
+def webhook():
+    print("webhook was hitted!")
+    return jsonify({"message": "webhook was hitted"})
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
